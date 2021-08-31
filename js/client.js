d99295516be32cef1ce2df17d013da9842fc6aae
Tyler Bach <Tbach2@my.wctc.edu>
2021-08-31 08:26:30 -0700

    initial commit

---
+++ js/client.js
@@ -0,0 +1,27 @@
+$(function () {
+    // preload audio
+    var toast = new Audio('media/toast.wav');
+
+    $('.code').on('click', function (e) {
+        e.preventDefault();
+
+        $("#product").text("Product: " + $(this).data('product'));
+        $("#code").text($(this).data('code'));
+
+        // first pause the audio (in case it is still playing)
+        toast.pause();
+        // reset the audio
+        toast.currentTime = 0;
+        // play audio
+        toast.play();
+        $('#toast').toast({ autohide: false }).toast('show');
+
+    });
+});
+
+$(document).keyup(function(e) {
+    if(e.key === "Escape"){
+        $('#toast').toast('hide');
+    }
+});
+
